e2d35a0d21efbfbcad5f7c9955baf284c569faba
osaretin frank <osaretin.frank10@gmail.com>
2022-08-01 20:54:06 +0100

    Installed Backend Dependencies

--- client/src/components/uploadPreview.jsx
+++ client/src/components/uploadPreview.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Typography, Box, Icon, Button } from "@mui/material"
+import { Container, Typography, Box, Button } from "@mui/material"
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 
