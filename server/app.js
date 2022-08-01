fed5ee0d0c5e5d1cd9696cd2d7effe6f309e7b04
osaretin frank <osaretin.frank10@gmail.com>
2022-08-01 21:07:30 +0100

    Server setup

---
+++ server/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const routes = require("./routes")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/api/image", routes)
+
+const PORT = process.env.PORT || 5500
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`)
+})
