054ed8fbe3dde9ce2b27e72faa0bcb568171ab39
osaretin frank <osaretin.frank10@gmail.com>
2022-08-02 12:07:27 +0100

    finished setting up cloudinary and multer

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const router = express.Router()
+const multer = require("multer")
+const clodinaryStorage = require("../cloudinary")
+const upload = multer({ storage: clodinaryStorage })
 
 router.get("/", (req, res, next) => {
   return res.status(200).json({ success: true, url: "https://cloudinary.com" })
+})
+
+router.post("/", upload.single("image"), (req, res, next) => {
+  try {
+    res.status(200).json({ success: true, image: req.file })
+  } catch (error) {
+    res.status(500).json({ success: false })
+  }
 })
 
 module.exports = router
